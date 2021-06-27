d3f5830c1a071cbbec8409ca9be64d6cf44c26a8
eagle2021 <chenwangying16@163.com>
2021-06-28 03:26:50 +0800

    '1111'

---
+++ cloud-provider-payment8001/src/main/java/com/eagle/my_spring_cloud/PaymentMain8001.java
@@ -0,0 +1,4 @@
+package com.eagle.my_spring_cloud;
+//@SpringBootApplication
+public class PaymentMain8001 {
+}
---
+++ src/main/java/com/eagle/my_spring_cloud/MySpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.my_spring_cloud;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class MySpringcloudApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(MySpringcloudApplication.class, args);
+//    }
+
+}
